5265ca1639e9c475962f22001e9f20fb406b74ee
xiaowei wan <xwan@evernote.com>
2019-06-02 13:46:36 -0700

    Add LC 1062

--- java/src/leetcode/Test.java
+++ java/src/leetcode/Test.java
@@ -2,7 +2,6 @@ package leetcode;
 
 public class Test {
   public static void main(String[] args) {
-    System.out.println(1e9+7);
     System.out.println("ab".substring(0,0));
 
   }
---
+++ java/src/leetcode/string/test.java
@@ -0,0 +1,4 @@
+package leetcode.string;
+
+public class test {
+}
