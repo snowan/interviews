64aad851d9db109cd37e3946a51a94982ff49c58
snowan <snowan@foxmail.com>
2019-06-02 22:23:38 -0700

    Update number_music_playlist.py

--- python/leetcode/LC920/number_music_playlist.py
+++ python/leetcode/LC920/number_music_playlist.py
@@ -1,9 +1,9 @@
 class Solution:
     def numMusicPlaylists(self, N: int, L: int, K: int) -> int:
-        mod = 10 ** 9 + 7;
-        dp = [[0 for i in range(N + 1)] for j in range(L + 1)];
-        dp[0][0] = 1;
+        mod = 10 ** 9 + 7
+        dp = [[0 for i in range(N + 1)] for j in range(L + 1)]
+        dp[0][0] = 1
         for i in range(1, L + 1):
             for j in range (1, N + 1):
-                dp[i][j] = (dp[i - 1][j - 1] * (N - j + 1) + dp[i - 1][j] * max(j - K, 0)) % mod;
-        return dp[L][N];
+                dp[i][j] = (dp[i - 1][j - 1] * (N - j + 1) + dp[i - 1][j] * max(j - K, 0)) % mod
+        return dp[L][N]
