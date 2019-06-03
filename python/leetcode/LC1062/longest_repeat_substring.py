d4ac3662eb2c1c822ce931369ef609ab4e1d5163
snowan <snowan@foxmail.com>
2019-06-02 22:24:31 -0700

    Update longest_repeat_substring.py

--- python/leetcode/LC1062/longest_repeat_substring.py
+++ python/leetcode/LC1062/longest_repeat_substring.py
@@ -1,13 +1,13 @@
 class Solution:
     def longestRepeatingSubstring(self, S: str) -> int:
-        n = len(S);
-        substr = set();
+        n = len(S)
+        substr = set()
         for leng in reversed(range(n + 1)):
             for l in range(n - leng + 1):
-                r = l + leng - 1;
-                temp = S[l: r + 1];
+                r = l + leng - 1
+                temp = S[l: r + 1]
                 if (temp in substr):
-                    return len(temp);
-                substr.add(temp);
-            substr.clear();
-        return 0;
+                    return len(temp)
+                substr.add(temp)
+            substr.clear()
+        return 0
