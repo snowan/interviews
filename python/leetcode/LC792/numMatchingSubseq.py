df76287f79f0dc10622447af16f933097cac685d
Bo Sun <bosun@pinterest.com>
2019-06-04 23:43:58 -0700

    update with iterator

--- python/leetcode/LC792/numMatchingSubseq.py
+++ python/leetcode/LC792/numMatchingSubseq.py
@@ -4,11 +4,8 @@ class Solution:
     def numMatchingSubseq(self, S: str, words: List[str]) -> int:
         word_dict = collections.defaultdict(list)
         for word in words:
-            word_dict[word[0]].append(word[1:])
+            word_dict[word[0]].append(iter(word[1:]))
         for c in S:
-            for word in word_dict.pop(c, []):
-                if not word:
-                    word_dict[''].append('')
-                else:
-                    word_dict[word[0]].append(word[1:])
-        return len(word_dict[''])
+            for word_it in word_dict.pop(c, []):
+                word_dict[next(word_it, None)].append(word_it)
+        return len(word_dict[None])
