d7d35f8651f1941674f2fac02dbb2e0ee04efc6d
Bo Sun <bosun@pinterest.com>
2019-06-05 17:45:53 -0700

    LC8

---
+++ python/leetcode/LC8/atoi.py
@@ -0,0 +1,24 @@
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        max_int = 2**31 - 1
+        min_int = -2**31
+        if not str:
+            return 0
+        str_strip = str.strip()
+        if not str_strip or \
+            (not str_strip[0].isdigit() and str_strip[0] != '+' and str_strip[0] != '-'):
+            return 0
+        pos = True
+        if not str_strip[0].isdigit():
+            pos = str_strip[0] == '+'
+            str_strip = str_strip[1:]
+        res = 0.0
+        for c in str_strip:
+            if not c.isdigit():
+                break
+            res = res * 10 + int(c)
+            if not pos and res * -1 < min_int:
+                return min_int
+            elif pos and res > max_int:
+                return max_int
+        return int(res) if pos else -int(res)
