b2a582d887b7d1beb54d9efc95ecd8a11fb89ac7
xiaowei wan <xwan@evernote.com>
2019-06-05 17:39:16 -0700

    remove unnecessary class

--- java/src/leetcode/Test.java
+++
@@ -1,8 +0,0 @@
-package leetcode;
-
-public class Test {
-  public static void main(String[] args) {
-    System.out.println("ab".substring(0,0));
-
-  }
-}
--- java/src/leetcode/string/test.java
+++
@@ -1,4 +0,0 @@
-package leetcode.string;
-
-public class test {
-}
