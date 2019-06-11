c664328320a0a58db7cb17f134529996b0419247
Bo Sun <bosun@pinterest.com>
2019-06-10 23:20:05 -0700

    LC30

---
+++ python/leetcode/LC30/solution.py
@@ -0,0 +1,36 @@
+from typing import List
+import collections
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        if not s or not words:
+            return []
+        w_num = len(words)
+        w_len = len(words[0])
+        w_count = collections.defaultdict(int)
+        for word in words:
+            w_count[word] += 1
+        res = []
+        for i in range(w_len):
+            temp_count = collections.defaultdict(int)
+            left = i
+            count = 0
+            for right in range(i, len(s) - w_len + 1, w_len):
+                temp_word = s[right: right + w_len]
+                if temp_word in w_count:
+                    temp_count[temp_word] += 1
+                    count += 1
+                    while temp_count[temp_word] > w_count[temp_word]:
+                        temp_count[s[left: left + w_len]] -= 1
+                        count -= 1
+                        left += w_len
+                    if count == w_num:
+                        count -= 1
+                        res.append(left)
+                        temp_count[s[left: left + w_len]] -= 1
+                        left += w_len
+                else:
+                    left = right + w_len
+                    temp_count.clear()
+                    count = 0
+        return res
