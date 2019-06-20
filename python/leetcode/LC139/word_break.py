61693d961c92836cac149f95e561f8b5e094d331
xiaowei wan <xwan@evernote.com>
2019-06-19 22:28:02 -0700

    Add LC 139

---
+++ python/leetcode/LC139/word_break.py
@@ -0,0 +1,11 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j: i] in words:
+                    dp[i] = True
+                    break
+        return dp[-1]
