0bbae60bc70b7d3da82d4ac506334c2efe53e0b0
xiaowei wan <xwan@evernote.com>
2019-07-25 22:21:14 -0700

    update compare expression trees

--- javasolution/src/main/java/google/CompareExpressionTrees.java
+++ javasolution/src/main/java/google/CompareExpressionTrees.java
@@ -1,8 +1,6 @@
 package google;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class CompareExpressionTrees {
 
@@ -15,11 +13,42 @@ public class CompareExpressionTrees {
     StringBuilder sb2 = new StringBuilder();
     getTreeExpression(tree2, sb2);
     System.out.println("tree2 " + sb2.toString());
-    Set<String> set1 = new HashSet<>(Arrays.asList(sb1.toString().split("\\+")));
-    Set<String> set2 = new HashSet<>(Arrays.asList(sb2.toString().split("\\+")));
-    boolean res = set1.size() == set2.size() && set1.containsAll(set2);
-    System.out.println(res);
-    return res;
+    Map<String, Integer> plus = new HashMap<>();
+    Map<String, Integer> minus = new HashMap<>();
+    // e.g. a + de + a + c - d
+    // after split: [a, de, a, c-d]
+    // after extract string into plus and minus map
+    // plus: [{a: 2}, {de, 1}, {c: 1}]
+    // minus: [{d: 1}]
+    String[] s = sb1.toString().split("\\+");
+    for (String str : s) {
+      int idx = str.indexOf("-");
+      String plusStr = str;
+      if (idx > 0) {
+        minus.put(str.substring(idx + 1), minus.getOrDefault(str.substring(idx + 1), 0) + 1);
+        plusStr = str.substring(0, idx);
+      }
+      plus.put(plusStr, plus.getOrDefault(plusStr, 0) + 1);
+    }
+
+    // compare
+    s = sb2.toString().split("\\+");
+    for (String str : s) {
+      int idx = str.indexOf("-");
+      String plusS = str;
+      if (idx > 0) {
+        if (!minus.containsKey(str.substring(idx + 1))) return false;
+        String tmp = str.substring(idx + 1);
+        minus.put(tmp, minus.get(tmp) - 1);
+        if (minus.get(tmp) == 0) minus.remove(tmp);
+        plusS = str.substring(0, idx);
+      }
+      if (!plus.containsKey(plusS)) return false;
+      plus.put(plusS, plus.get(plusS) - 1);
+      if (plus.get(plusS) == 0) plus.remove(plusS);
+    }
+
+    return minus.size() == 0 && plus.size() == 0;
   }
 
   private static void getTreeExpression(TreeNode node, StringBuilder sb) {
@@ -40,11 +69,11 @@ public class CompareExpressionTrees {
   }
 
   public static void main(String[] args) {
-    String[] nodes1 = {"+", "a", "+", null, null, "c", "de"}; //{"+", "a", "b"}; //
+    String[] nodes1 = {"+", "+", "+", "a", "de", "a", "c"}; //{"+", "a", "b"}; //
     String[] nodes2 = {"+", "+", "de", "a", "c"}; //{"+", "b", "a"}; //
     TreeNode r1 = CompareExpressionTrees.buildTree(nodes1, new TreeNode(null), 0);
     TreeNode r2 = CompareExpressionTrees.buildTree(nodes2, new TreeNode(null), 0);
-    CompareExpressionTrees.equalExpressionTrees(r1, r2);
+    System.out.println(CompareExpressionTrees.equalExpressionTrees(r1, r2));
   }
 }
 
