6c7713c9d3be94751f4a9f4c473408c96660401c
snowan <6018815+snowan@users.noreply.github.com>
2021-02-09 22:19:39 -0800

    Update 538.convert-bst-to-greater-tree.java

--- 2021/java/538.convert-bst-to-greater-tree.java
+++ 2021/java/538.convert-bst-to-greater-tree.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    // recursive
     public TreeNode convertBST(TreeNode root) {
         if (root == null) return null;
         helper(root, new int[1]);
@@ -25,5 +26,28 @@ class Solution {
         node.val += sum[0];
         sum[0] = node.val;
         helper(node.left, sum);
+    }
+    
+    // iterative
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) return null;
+        int sum = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            // put right treenode into stack
+            while (node != null) {
+                stack.push(node);
+                node = node.right;
+            }
+            // calculate sum, and reset node val
+            node = stack.pop();
+            sum += node.val;
+            node.val = sum;
+            
+            // after set right node, set node to left, 
+            node = node.left;
+        }
+        return root;
     }
 }
