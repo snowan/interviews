bb0cc2229a80d6ef1b982ffd275e52fcdbdb9330
snowan <6018815+snowan@users.noreply.github.com>
2021-02-17 20:28:02 -0800

    Create 11.container-with-most-water.py

---
+++ 2021/python/11.container-with-most-water.py
@@ -0,0 +1,46 @@
+"""
+11. Container With Most Water
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+Notice that you may not slant the container.
+
+Example 1:
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+
+Example 3:
+
+Input: height = [4,3,2,1,4]
+Output: 16
+
+Example 4:
+
+Input: height = [1,2,1]
+Output: 2
+ 
+
+Constraints:
+
+n == height.length
+2 <= n <= 3 * 104
+0 <= height[i] <= 3 * 104
+
+"""
+def maxArea(self, height: List[int]) -> int:
+    if not height or len(height) < 2:
+        return 0
+    res, l, r = 0, 0, len(height) - 1
+    while l < r:
+        res = max(res, min(height[l], height[r]) * (r - l))
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+
+    return res
