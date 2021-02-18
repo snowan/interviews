15729dc1281bcd13d55ffd33a39c7e5a0d574c3f
snowan <6018815+snowan@users.noreply.github.com>
2021-02-17 22:02:13 -0800

    Lc11 (#46)
    * Create 11.container-with-most-water.py
    
    * Create 11.container-with-most-water.java

---
+++ 2021/java/11.container-with-most-water.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) return 0;
+        int res = 0;
+        int l = 0;
+        int r = height.length - 1;
+        while (l < r) {
+            res = Math.max(res, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        
+        return res;
+    }
+}
