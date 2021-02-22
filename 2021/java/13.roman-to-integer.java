9fe5fa58b34761c2923573b42b08da709d20fe35
snowan <6018815+snowan@users.noreply.github.com>
2021-02-21 16:57:43 -0800

    Lc13 (#49)
    * Create 13.roman-to-integer.py
    
    * Create 13.roman-to-integer.java

---
+++ 2021/java/13.roman-to-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int prev = map.get(s.charAt(0));
+        int sum = 0;
+        int next = 0;
+        for (int i = 1; i < s.length(); i++) {
+            next = map.get(s.charAt(i));
+            sum = prev < next ? sum - prev : sum + prev
+            // reset prev
+            prev = next;
+        }
+        
+        return sum + prev;
+    }
+}
