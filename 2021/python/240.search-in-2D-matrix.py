8d4fcba5e71a9124d43a6771342ee87ef09beeaf
snowan <6018815+snowan@users.noreply.github.com>
2021-02-23 22:26:56 -0800

    Update 240.search-in-2D-matrix.py
    Fix comments

--- 2021/python/240.search-in-2D-matrix.py
+++ 2021/python/240.search-in-2D-matrix.py
@@ -31,7 +31,7 @@ All the integers in each column are sorted in ascending order.
 
 """
 def searchMatrix(matrix: List[List[int]], target: int) -> bool:
-    if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
+    if not matrix or not matrix[0]:
         return False
     rows, cols = len(matrix), len(matrix[0])
     r, c = 0, cols - 1
