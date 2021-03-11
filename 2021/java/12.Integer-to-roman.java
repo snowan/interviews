879f3ade89df7ad67bff67a911a9a8a21decf115
snowan <6018815+snowan@users.noreply.github.com>
2021-03-10 22:14:22 -0800

    Create 12.Integer-to-roman.java

---
+++ 2021/java/12.Integer-to-roman.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String intToRoman(int num) {
+        if (num < 1 || num > 3999) return "";
+        StringBuilder sb = new StringBuilder();
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	      int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        int idx = 0;
+        while (num > 0) {
+            while (num >= values[idx]) {
+                num -= values[idx];
+                sb.append(roman[idx]);
+            }
+            idx++;
+        }
+        return sb.toString();
+    }
+
+    public static String intToRoman(int num) {
+        String M[] = {"", "M", "MM", "MMM"};
+        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+}
