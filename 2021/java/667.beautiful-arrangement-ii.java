c0c1081570b00a9af00feb7fe9b12e93c8168e53
snowan <6018815+snowan@users.noreply.github.com>
2021-04-12 21:33:13 -0700

    Create 667.beautiful-arrangement-ii.java

---
+++ 2021/java/667.beautiful-arrangement-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] constructArray(int n, int k) {
+        int[] res = new int[n];
+        for (int i = 0, l = 1, r = n; l <= r; i++)
+            res[i] = k > 1 ? (k-- % 2 != 0 ? l++ : r--) : l++;
+        return res;
+    
+    }
+}
+/*
+reference: https://leetcode.com/problems/beautiful-arrangement-ii/discuss/106948/C%2B%2B-Java-Clean-Code-4-liner
+if you have n number, the maximum k can be n - 1;
+if n is 9, max k is 8.
+This can be done by picking numbers interleavingly from head and tail,
+
+// start from i = 1, j = n;
+// i++, j--, i++, j--, i++, j--
+
+i: 1   2   3   4   5
+j:   9   8   7   6
+out: 1 9 2 8 3 7 4 6 5
+dif:  8 7 6 5 4 3 2 1
+Above is a case where k is exactly n - 1
+When k is less than that, simply lay out the rest (i, j) in incremental
+order(all diff is 1). Say if k is 5:
+
+     i++ j-- i++ j--  i++ i++ i++ ...
+out: 1   9   2   8    3   4   5   6   7
+dif:   8   7   6   5    1   1   1   1 
+*/
