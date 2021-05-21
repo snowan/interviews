71aac9bb5fc54736d5765e12cd9521417e904e4a
snowan <6018815+snowan@users.noreply.github.com>
2021-05-20 23:14:07 -0700

    Create 102.binary-tree-level-traverse-order.java

---
+++ 2021/java/102.binary-tree-level-traverse-order.java
@@ -0,0 +1,42 @@
+class Solution {
+    // BFS
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode curr = queue.poll();
+                temp.add(curr.val);
+                if (curr.left != null) queue.add(curr.left);
+                if (curr.right != null) queue.add(curr.right);
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+    
+    // DFS
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(root, 0, res);
+        return res;
+    }
+    private void dfs(TreeNode node, int depth, List<List<Integer>> res) {
+        if (node == null) return;
+        if (depth >= res.size()) {
+            List<Integer> curr = new ArrayList<>();
+            curr.add(node.val);
+            res.add(curr);
+        } else {
+            res.get(depth).add(node.val);
+        }
+        dfs(node.left, depth + 1, res);
+        dfs(node.right, depth + 1, res);
+    }
+    
+}
