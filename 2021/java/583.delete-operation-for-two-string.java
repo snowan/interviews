af849adfdb0f06130de84ac3b537311f45a9ceb7
snowan <6018815+snowan@users.noreply.github.com>
2021-05-31 21:15:21 -0700

    Create 583.delete-operation-for-two-string.java (#134)
    * Create 583.delete-operation-for-two-string.java
    
    * Create 583.delete-operation-for-two-string.py

---
+++ 2021/java/583.delete-operation-for-two-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    // find longest common subarray 
+    // min operations = total len (n + m - 2 * longest)
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) dp[i][j] = 0;
+                else {
+                    if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1; 
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+        return n - dp[n][m] + m - dp[n][m];
+    }
+}
---
+++ 2021/python/583.delete-operation-for-two-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n, m = len(word1), len(word2)
+        dp = [[0] * (m + 1) for i in range(n + 1)]
+        for i in range(n):
+            for j in range(m):
+                dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j], dp[i][j] + (word1[i] == word2[j]))
+        return n - dp[n][m] + m - dp[n][m]
